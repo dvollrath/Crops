af9ce135b10653696a8287bd6ee21b338c5e7755
Dietz Vollrath <dietz.vollrath@gmail.com>
2017-11-14 19:39:50 -0500

    Update with long slides

--- Replicate/Crops_Data_Master.r
+++ Replicate/Crops_Data_Master.r
@@ -63,7 +63,7 @@ setwd(refdir)
 #source(file.path(codedir,'Crops_Data_GAEZ_Cult.r')) # Cultivated area
 #source(file.path(codedir,'Crops_Data_KG_Count.r')) # KG zone data
 #source(file.path(codedir,'Crops_Data_Earthstat.r')) # Production data
-source(file.path(codedir,'Crops_Data_GRUMP.r')) # Population data
+#source(file.path(codedir,'Crops_Data_GRUMP.r')) # Population data
 
 #######################################################################
 ## Call programs for pre-1500 processing
